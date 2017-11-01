37581435c5dff4c5b8e21c3b164e915416155cb7
pasted <tufpaste@hotmail.com>
2017-11-01 10:16:54 +0000

    prep for upload to github

---
+++ lib/association_store.py
@@ -0,0 +1,6 @@
+class AssociationStore():
+
+   def __init__(self, response):
+      for r in response['data']:
+         print(r)
+	
--- open_targets_query.py
+++ open_targets_query.py
@@ -1,23 +1,45 @@
-import argparse
-import requests
+"""
+Main entry point for the Open Targets Query script.
+Returns a JSON response from given OPen Targets endpoint, and produces summary stats
+"""
 from pprint import pprint
+import argparse
+from lib.association_store import AssociationStore
+from opentargets import OpenTargetsClient
 
-class OpenTargetsQuery():
-	
-   def request(arg):
-      #https://www.targetvalidation.org/api/latest/public/search
-      reply = requests.get('https://www.targetvalidation.org/api/latest/public/association/filter', params={"q":arg, "size":1})
-      pprint(reply.json())
+class OpenTargetsQuery(object):
+    '''Open Targets Query'''
+    def __init__(self):
+        args = []
+        parser = argparse.ArgumentParser(description='OpenTargets client')
+        parser.add_argument('-t', '--target', dest='target',
+   	                        help='Run analysis for a target, for example -t ENSG00000197386')
+        parser.add_argument('-d', '--disease', dest='disease',
+   	                        help='Run analysis for a disease, for example -d Orphan_399')
+        parser.add_argument('--test')
 
+        args = parser.parse_args()
+        self.request(args.target, "target")
+
+    def request(self, arg, query_type):
+        """
+        Requests response from given endpoint
+        Args:
+            arg (str): Given command line argument to query endpoint with
+            query_type (str): Parameter for RESTful api query
+            url (str): Given RESTful api endpoint
+        Returns:
+            response: A JSON response
+        """
+        ot = OpenTargetsClient()
+        a_for_target = ot.get_associations_for_target(arg)
+        for a in a_for_target:
+            print(a)
+        #reply = requests.get(url,
+        #                     params={query_type:arg})
+        #pprint(reply.json())
+        #AssociationStore(reply)
+        #len(r['data'])
 
 if __name__ == '__main__':
-   parser = argparse.ArgumentParser(description='OpenTargets client')
-   parser.add_argument('-t', '--target', dest='target', help='Run analysis for a target, for example -t ENSG00000197386')
-   parser.add_argument('-d', '--disease', dest='disease', help='Run analysis for a disease, for example -d Orphan_399')
-   parser.add_argument('--test')
-   
-   args = parser.parse_args()
-      
-   query = OpenTargetsQuery
-   query.request(args.target)
-      
+    OpenTargetsQuery()
